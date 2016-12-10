29cdfc69e387afc7a05fab9bbc305ff97d443734
Dero <deroman102@gmail.com>
2016-12-10 15:42:00 +0800

    Test Commit And Push

--- src/crawler/example/FacebookExample.java
+++ src/crawler/example/FacebookExample.java
@@ -25,7 +25,7 @@ public class FacebookExample {
 		// [query sample]
 		// search?fields=name,id,likes,talking_about_count&limit=1000&q=靠北&type=page
 		String uri = 
-				"https://graph.facebook.com/v2.5"
+				"https://graph.facebook.com/v2.8"
 				+ "/search?q=%E9%9D%A0%E5%8C%97&type=page&limit=10&fields=id,name,talking_about_count,fan_count,likes"
 				+ "&access_token=EAACEdEose0cBAAZChcZAJ7Azw8XYWZA3FGdPEZBCKT6wglgmCqoZAl7iRIGR3fQTVAh4pZB7lOzZAvBzlPh5RDSBFqiIfBtI5FAe0OXSFuRlKE3TOXZCZCpXZBDOcTe7l8ZA3a7LU2ZC7VFxKbOwZCoscRm5JBTvfNU3Cyv0nkCZAS9TrxPQZDZD";
 
@@ -54,10 +54,11 @@ public class FacebookExample {
 		for( Element data: elems ){
 			String id = data.select("id").text();
 			String name = data.select("name").text();
-			String likes = data.select("likes").text();
+			String likes =data.select("likes").text();
+			String fans =data.select("fan_count").text();
 			String talking_about_count = data.select("talking_about_count").text();
 			
-			output += id+",\""+name+"\","+likes+","+talking_about_count+"\n";
+			output += id+",\""+name+"\","+fans+","+","+talking_about_count+likes+"\n";
 		}
 		
 		System.out.println( output );
